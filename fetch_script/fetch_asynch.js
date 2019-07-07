fcea4476a0f06d1be20611b3b8655801847af279
Benjamin <benjamin.seibel@gmx.de>
2019-07-08 01:05:17 +0200

    asnyc rewrite

--- fetch_script/fetch_asynch.js
+++ fetch_script/fetch_asynch.js
@@ -10,42 +10,160 @@ const config = require('./config.json'),
   db = require('./db');
 
 
-async function callAllApis () {
+async function main () {
   await Promise.all(config.map(async (el) => {
+
     const client = await ttn.data(el.appId, el.key);
 
     client
       .on('uplink', async function(devID, payload){
+        console.log('received ping by ' + devID);
 
-        const AppExists = await checkForApp(el);
+        let app = await appExists(el.uniqueId);
 
-        console.log('Received uplink from ', devID);
-        console.log(payload);
+        if (app) {
+          console.log('App found! Looking for Device');
+          let device = await deviceExists(app._id, devID);
+
+          if (device) {
+            // Device found, saving only payload
+            console.log('Device found');
+            await savePayload(device._id, payload.metadata.time, payload.payload_fields);
+            console.log('Payload saved');
+
+          } else {
+            // device not found, creating new device and save payload;
+            console.log('Device not found');
+            let device = await saveDevice(app._id, devID, payload.hardware_serial);
+            await savePayload(device._id, payload.metadata.time, payload.payload_fields);
+          }
+        } else {
+          console.log('App not found!');
+          await saveApp(el);
+
+        }
+
 
       });
   }));
 }
 
-callAllApis()
+/**
+ * Checks if an App is already present in the MongoDB.
+ *
+ * @param {String} id Unique ID of the TTN App
+ * @returns {Object} If present, returns app. If not, returns undefined
+ */
+const appExists = (id) => {
+  return AppModel.findOne({'appId': id})
+    .exec()
+    .then((app) =>{
+      return app;
+    })
+    .catch((err) => {
+      return err;
+    });
+};
+
+/**
+ * Checks if a device is already present in the MongoDB.
+ *
+ * @param {String} id Unique ID of the TTN App
+ * @returns {Object} If present, returns app. If not, returns undefined
+ */
+
+const deviceExists = (appId, devId) => {
+  return DeviceModel.findOne({'parentApp': appId, 'devId': devId})
+    .exec()
+    .then((device) => {
+      return device;
+    })
+    .catch((err) => {
+      return err;
+    });
+};
+
+/**
+ * Saves a new TTN App to MongoDB
+ * @param {Object} data The data object for the app.
+ */
+function saveApp(el) {
+  let appData = {
+    'appId': el.appId,
+    'uniqueId': el.uniqueId,
+    'description:': el.description,
+    'locationName': el.locationName
+  };
+
+  let App = new AppModel(appData);
+
+  return App.save()
+    .then((app) => {
+      console.log('Saved new App ' + app.appId + 'to the database.');
+      return app;
+    }).catch((err) => {
+      return err;
+    });
+}
+
+/**
+ *
+ * @param {*} appId The MongoDB ID of the parent app;
+ * @param {*} devId The device ID
+ * @param {*} hardwareSerial The hardware serial of the device
+ */
+function saveDevice(appId, devId, hardwareSerial) {
+  let deviceData = {
+    'parentApp': appId,
+    'devId': devId,
+    'hardwareSerial': hardwareSerial
+  };
+
+  let Device = new DeviceModel(deviceData);
+
+  return Device.save()
+    .then((device) => {
+      return device;
+    }).catch((err) => {
+      return err;
+    });
+
+}
+
+
+/**
+ * Saves a payload with the according device, time and values
+ *
+ * @param {String} devId The Device ID
+ * @param {String} time Timestamp as a String
+ * @param {Object} fields The payload value object
+ */
+
+function savePayload(devId, time, fields){
+
+  let payloadData = {
+    'parentDevice': devId,
+    'timestamp': time,
+    'payload': fields
+  };
+
+  let Pay = new PayloadModel(payloadData);
+  return Pay.save()
+    .then((pay) => {
+      console.log('Payload added: ' + pay);
+    }).catch((err) => {
+      return err;
+    });
+
+}
+
+
+/**
+ * Main function
+ */
+
+main()
   .catch(function (err) {
     console.error(err);
     process.exit(1);
   });
-
-async function checkForApp(data) {
-  AppModel.findOne({'uniqueId': data.uniqueId}, function(err, app) {
-    if (!app) {
-      console.log('App not found, creating new entry');
-      //createNewApp(data);
-      console.log('I would enter this? ' + data);
-      return;
-    } else {
-      console.log('App found!');
-      return;
-    }
-  });
-}
-
-async function createNewApp(data) {
-
-}
